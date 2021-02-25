84bcdfe9b53753d36bbf01c307b0d5416e32b76b
djangoJobsProject <tarankohli86@gmail.com>
2021-02-25 16:06:59 +0530

    myfirstcommit

---
+++ secondProject/testApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import datetime
+# Create your views here.
+
+def timeinfo(request):
+    
+    date = datetime.datetime.now()
+    msg="<h1>Hello friends good evening !!!</h1><hr>"
+    msg=msg + "<h1>Now the server time is :"+str(date)+"</h1>"
+    return HttpResponse(msg)
+    
